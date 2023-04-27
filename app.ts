d21c756764d464d3b98423b689e7a928d242b158
nitipon <nitipon.apaisri@gmail.com>
2023-04-27 17:07:45 +0200

    1st commit

---
+++ app.ts
@@ -0,0 +1,26 @@
+import 'dotenv/config'
+import express, {Express, Request, Response } from 'express';
+import logger from './middlewares/logger';
+const app: Express = express();
+const port = process.env.PORT || 1997;
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+// app.use(logger)
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello World!');
+});
+const run = async () => { 
+    try {
+        app.listen(port, () => {
+            console.log(`Server is running on port ${port}`);
+        });
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+if(process.env.NODE_ENV !== 'test') { 
+    run();
+}
+export default app;
