cc7411857901e3fca2af1e495a6841fbbf21b36d
nitipon <nitipon.apaisri@gmail.com>
2023-05-16 22:17:09 +0200

    Added a utility method for calculate ethereum trasaction fee

--- tests/utils.test.ts
+++ tests/utils.test.ts
@@ -1,10 +1,13 @@
 import { describe, expect, test } from "@jest/globals";
-import { weiToGwei, weiToEth } from "../utils/converts";
+import { weiToGwei, weiToEth, calcTransactionFee } from "../utils/converts";
 describe("Utils", () => {
     test("10^18 wei should be 1 ETH", () => {
         expect(weiToEth("1000000000000000000", false)).toBe(1);
     });
     test("10^18 wei should be 10^9 gwei ", () => {
         expect(weiToGwei("1000000000000000000")).toBe(1000000000);
+    });
+    test("should return 0.000612443339624048 when gas used equal = 45896 and gas price equal 13344155038", async () => {
+        expect(calcTransactionFee("13344155038", "45896")).toBe(0.000612443339624048);
     });
 });
--- utils/converts.ts
+++ utils/converts.ts
@@ -1,6 +1,6 @@
 import { BigNumber } from "alchemy-sdk";
 
-export const weiToEth = (wei: string, isBigNumber: boolean) => {
+const weiToEth = (wei: string, isBigNumber: boolean) => {
     if (isBigNumber) {
         const bn = BigNumber.from(wei).toString();
         const calc = Number(bn) / 10 ** 18;
@@ -11,12 +11,17 @@ export const weiToEth = (wei: string, isBigNumber: boolean) => {
     }
 };
 
-export const weiToGwei = (gwei: string) => {
+const weiToGwei = (gwei: string) => {
     const calc = Number(gwei) / 10 ** 9;
     return Number(calc.toFixed(4));
 };
 
-export const tokenBalanceFormat = (token: any, symbol: string) => {
+const tokenBalanceFormat = (token: any, symbol: string) => {
     const tokenBalance = `${BigNumber.from(token.tokenBalance).toString()} ${symbol}`;
     return tokenBalance;
 };
+const calcTransactionFee = (gasPrice: string, gasUsed: string) => {
+    const calc = (Number(gasPrice) * Number(gasUsed)) / 10 ** 18;
+    return Number(calc);
+};
+export { weiToEth, weiToGwei, tokenBalanceFormat, calcTransactionFee };
